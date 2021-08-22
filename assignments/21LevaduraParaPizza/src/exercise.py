659d123045ec22c7358e19ca34f5c7f8a5205119
SamyEa12 <77683636+SamyEa12@users.noreply.github.com>
2021-08-21 22:58:59 -0500

    Update exercise.py

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,7 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    harina=float(input("Dame la harina en gramos: "))
+    levadura=(harina*50)/1000
+    print("Necesitas estos gramos de levadura: " + str(levadura))
     pass
-
 
 if __name__ == '__main__':
     main()
