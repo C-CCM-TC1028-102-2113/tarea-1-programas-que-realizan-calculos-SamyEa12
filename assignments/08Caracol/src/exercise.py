b5f46859ac4bb348b9d33a7538b4b01cf808055c
SamyEa12 <77683636+SamyEa12@users.noreply.github.com>
2021-08-21 21:24:38 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-minutos = float(input("Dame los minutos: "))
-segundos = minutos*60
-Centímentros = (segundos*5.7)/10
-print("Centímentros recorridos: " + Centímentros)
+    minutos = float(input("Dame los minutos: "))
+    segundos = minutos*60
+    Centímentros = (segundos*5.7)/10
+    print("Centímentros recorridos: " + Centímentros)
+    pass
+
 if __name__ == '__main__':
     main()
