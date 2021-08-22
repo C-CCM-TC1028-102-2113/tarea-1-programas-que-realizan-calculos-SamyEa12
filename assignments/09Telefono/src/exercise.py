ae74481e1b5eceb1260df34201d0285d4e1406d2
SamyEa12 <77683636+SamyEa12@users.noreply.github.com>
2021-08-21 22:05:34 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,19 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
     #Leer los datos
-    mensajes=int(input("Dame el número de mensajes: "))
+    mensajes=float(input("Dame el número de mensajes: "))
     megas=float(input("Dame el número de megas: "))
-    minutos=int(input("Dame el número de minutos: "))
+    minutos=float(input("Dame el número de minutos: "))
 
-    pmen=mensajes*0.80
-    pmeg=megas*0.80
-    pmin=minutos*0.80
+    pmen=mensajes*0.8
+    pmeg=megas*0.8
+    pmin=minutos*0.8
     
     precio=pmen+pmeg+pmin
     
     print("El costo mensual es: " + str(precio)) 
     pass
-
 
 if __name__ == '__main__':
     main()
