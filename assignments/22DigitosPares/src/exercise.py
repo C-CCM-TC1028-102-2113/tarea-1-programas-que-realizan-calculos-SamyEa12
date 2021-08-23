047c12b2b346224dbb1a69ff87a1a73d30e58ef2
SamyEa12 <77683636+SamyEa12@users.noreply.github.com>
2021-08-22 22:51:44 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,29 +1,27 @@
 def main():
     #escribe tu código abajo de esta línea
-    print("Introduce un número entero de 4 dígitos")
-    n = int(input())
-    if (n%2==0):
-        print ("- El cuarto dígito es par")
-    else:
-        print ("- El cuarto dígito es impar")
-    n1 = n//10
-    print (n1)
+    n=int(input("Dame un número: "))
+    n1=n//1000
+    n2=n//100
+    n3=n//10
     if (n1%2==0):
-        print ("- El tercer dígito es par")
+        d1=1
     else:
-        print ("- El tercer dígito es impar")
-    n2 = n1//10
-    print(n2)
+        d1=0
     if (n2%2==0):
-        print ("- El segundo dígito es par")
+        d2=1
     else:
-        print ("- El segundo dígito es impar")
-    n3 = n2//10
-    print(n3)
+        d2=0
     if (n3%2==0):
-        print ("- El primero dígito es par")
+        d3=1
     else:
-        print ("- El primer dígito es impar")
+        d3=0
+    if (n%2==0):
+        d4=1
+    else:
+        d4=0
+    df=d1+d2+d3+d4
+    print("El número de dígitos pares es: " + str(df))
     pass
 
 if __name__ == '__main__':
